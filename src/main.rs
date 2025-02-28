758efe9c3b84dd4e3589ae11e4a8f50614217da0
ahaoboy <504595380@qq.com>
2025-02-28 10:26:57 +0800

    profile.release

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,11 @@ use std::ffi::{CStr, CString};
 use std::ptr::null_mut;
 use mujs_one::{
   js_State, js_dostring, js_newcfunction, js_newstate, js_pushundefined, js_setglobal,
-  js_tostring, JS_STRICT,
+  js_tostring
 };
 
 unsafe extern "C" fn print(j: *mut js_State) {
-    let name = js_tostring(j as *mut js_State, JS_STRICT.try_into().unwrap());
+    let name = js_tostring(j as *mut js_State, -1);
     let s = CStr::from_ptr(name).to_string_lossy();
     println!("{}", s);
     js_pushundefined(j as *mut js_State);
@@ -25,7 +25,7 @@ unsafe extern "C" fn print(j: *mut js_State) {
 
 fn eval_code(code: &str) -> String {
     unsafe {
-        let j = js_newstate(None, null_mut(), JS_STRICT.try_into().unwrap());
+        let j = js_newstate(None, null_mut(), 0);
         let log_fn: Option<unsafe extern "C" fn(*mut js_State)> = Some(print);
         let log_name = CString::new("print").unwrap();
         js_newcfunction(j as *mut js_State, log_fn, log_name.as_ptr(), 1);
@@ -41,6 +41,7 @@ fn main() {
     if let Some(path) = std::env::args().nth(1) {
       let buffer = std::fs::read_to_string(&path).expect("failed to read file");
       eval_code(&buffer);
+      return;
     }
     println!("mujs-one <PATH>");
 }
